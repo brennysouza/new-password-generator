a6568863aa9ee0e10ac575999b6387cf524a68e2
Brenny Souza <106251815+brennysouza@users.noreply.github.com>
2023-06-30 16:20:39 -0400

    finished commenting js code

--- Develop/script.js
+++ Develop/script.js
@@ -25,36 +25,54 @@ function writePassword() {
 }
 
 // Add event listener to generate button
+// This code adds an event listener to the generate button that will allow it to be clicked and function properly.
+// This will initilize the writePassword function found within the paranthesis. 
 generateBtn.addEventListener("click", writePassword); 
 
+// This code is the generate password function and initializes the user to enter the desired password character length from 8-128 characters. 
+// The value is stored in the character length variable below.
+// The prompt function allows for a message to appear for the user to read which gives instruction of how many characters to choose. No more than 128 and no less than 8.
 function generatePassword() {
   var characterLength = prompt('How many characters would you like your password to be? Must contain anywhere between 8-128 characters.')
+  
+  // This code ensures that the entered length will be between 8 - 128 characters. If the number is selected is outside of this range, an alert will show up to include a proper number. 
   if (characterLength < 8 || characterLength > 128 || isNaN(characterLength)) {
     alert('Invalid! Password must contain a length anywhere between 8-128 characters. Please do not include letters when choosing password length.');
     return generatePassword();
   }
+  // This code allows users use to have uppercase characters in generated password if selected in the prompt.
+  // These are added to the answer array. 
   var useUpperCase = confirm('Would you like your password to have uppercase letters?');
   if (useUpperCase === true) {
     answer.push(upperCase);
   }
+  // This code allows users use to have lowercase characters in generated password if selected in the prompt.
+  // These are added to the answer array. 
   var useLowerCase = confirm('Would you like your password to have lower case letters?');
   if (useLowerCase === true) {
     answer.push(lowerCase);
   }
+  // This code allows users use to have numerical characters in generated password if selected in the prompt.
+  // These are added to the answer array. 
   var useNumber = confirm('Would you like your password to have numbers?'); 
   if (useNumber === true) {
     answer.push(number);
   }
+  // This code allows users use to have symbol characters in generated password if selected in the prompt.
+  // These are added to the answer array. 
   var useSymbol = confirm('Would you like your password to have symbols?');
   if (useSymbol === true) {
     answer.push(symbol);
   }
 
+  // This code alerts users if they do not choose ANY criteria for a new password that atleat one criteria must be chosen in order for a password to be generated. 
   if (useUpperCase == false && useLowerCase == false && useNumber == false && useSymbol == false) {
     alert('Invalid! Please select ATLEAST one character type for password.');
     return generatePassword();
   }
 
+  // This code allows users to not have the upper case letter criteria if chosen and will not display once generated password is revealed. 
+  // If user excludes this from their criteria options, it will be removed from the answer array.
   if (useUpperCase === false) {
     var index = answer.indexOf(upperCase);
     if (index !== -1) {
@@ -62,6 +80,8 @@ function generatePassword() {
     }
   }
 
+  // This code allows users to not have the lower case letter criteria if chosen and will not display once generated password is revealed. 
+  // If user excludes this from their criteria options, it will be removed from the answer array.
   if (useLowerCase === false) {
     var index = answer.indexOf(lowerCase);
     if (index !== -1) {
@@ -69,6 +89,8 @@ function generatePassword() {
     }
   }
 
+  // This code allows users to not have the numbers criteria if chosen and will not display once generated password is revealed. 
+  // If user excludes this from their criteria options, it will be removed from the answer array.
   if (useNumber === false) {
     var index = answer.indexOf(number);
     if (index !== -1) {
@@ -76,6 +98,8 @@ function generatePassword() {
     }
   }
 
+  // This code allows users to not have the symbol characters criteria if chosen and will not display once generated password is revealed. 
+  // If user excludes this from their criteria options, it will be removed from the answer array.
   if (useSymbol === false) {
     var index = answer.indexOf(symbol);
     if (index !== -1) {
@@ -83,19 +107,23 @@ function generatePassword() {
     }
   }
 
+  // Thise code logs the answer array or the generated password to the console. 
   console.log(answer);
 
   //initializing password 
   var password = "";
 
   // This code generates random characters for password 
+  // This for loop code will generate random characters for the password. The for loop will generate a password passwed on the amount chosne in the character length variable.
+  // A random character sets will be selected from the answer array based off the Math.random method.
   for (var i = 0; i < characterLength; i++) {
   // Write the logic in here
     var randomAnswer = answer[Math.floor(Math.random() * answer.length)];
     var randomCharacter = randomAnswer[Math.floor(Math.random() * randomAnswer.length)];
     password += randomCharacter;
   }
-  // password = password + randomCharacter 
+
+  // This line of code will return the randomly generated password from the generate password function after the button is clicked, characters length chose, and criteria is selected. 
   return password;
   }
 
